358d4f6f207fb93cd38005b5db62ef633d26c909
Yi Zhang <zhang9@outlook.de>
2021-02-24 23:27:45 +0100

    Initial commit

---
+++ forward_template.jl
@@ -0,0 +1,161 @@
+using MAT
+using Plots
+using MATLAB
+using Dates
+const USE_GPU = false  # Use GPU? If this is set false, then no GPU needs to be available
+using ParallelStencil
+using ParallelStencil.FiniteDifferences2D
+@static if USE_GPU
+    @init_parallel_stencil(CUDA, Float64, 2);
+else
+    @init_parallel_stencil(Threads, Float64, 2);
+end
+include("./seismic2D_function.jl");
+## load image
+vp=ones(600,600)*2000;
+nx,nz=size(vp);
+
+##
+# dimensions
+dt=10^-3;
+dx=10;
+dz=10;
+nt=2000;
+nx=size(vp,1);
+nz=size(vp,2);
+
+X=(1:nx)*dx;
+Z=-(1:nz)*dz;
+
+# PML layers
+lp=20;
+
+# PML coefficient, usually 2
+nPML=2;
+
+# Theoretical coefficient, more PML layers, less R
+# Empirical values
+# lp=[10,20,30,40]
+# R=[.1,.01,.001,.0001]
+Rc=.0001;
+
+# generate empty density
+rho=ones(nx,nz)*1;
+#rho=rho.*A;
+# Lame constants for solid
+mu=rho.*(vp/sqrt(3)).^2;
+lambda=rho.*vp.^2;
+##
+mutable struct C2
+    C11
+    C13
+    C33
+    C55
+    rho
+end
+C=C2(lambda+2*mu,lambda,lambda+2*mu,mu,rho);
+
+# source
+# magnitude
+M=2.7;
+
+s_s1=[200];
+s_s3=ones(Int32,size(s_s1))*200;
+##
+# point interval in time steps
+plot_interval=100;
+
+p2= @__FILE__;
+if isdir(chop(p2,head=0,tail=3))==0
+    mkdir(chop(p2,head=0,tail=3))
+end;
+##for source_code=1
+@time begin
+source_code=1;
+    # source locations
+    s1=s_s1[source_code];
+    s3=s_s3[source_code];
+
+    # source frequency [Hz]
+    freq=5;
+
+    # source signal
+    singles=rickerWave(freq,dt,nt,M);
+
+    # give source signal to x direction
+    src1=zeros(Float32,nt,1);
+    src1=1*repeat(singles,1,length(s3));
+
+    # give source signal to z direction
+    src3=src1;
+    src3=1*repeat(singles,1,length(s3));
+
+    # receiver locations [m]
+    r1=3;
+    r3=3;
+
+    s1t=dx .*s1;
+    s3t=maximum(Z)-dz .*s3;
+    r1t=dx .*r1;
+    r3t=maximum(Z)-dz .*r3;
+
+    # source type. 'D' for directional source. 'P' for P-source.
+    source_type='P';
+
+    # plot source
+    plot_source=1;
+
+    # path for this source
+    path=string(chop(p2,head=0,tail=3),"/source_code_",(source_code),"/");
+
+    # save wavefield
+    save_wavefield=0;
+    ## pass parameters to solver
+    v1,v3,R1,R3,P=VTI_2D(dt,dx,dz,nt,nx,nz,
+    X,Z,
+    r1,r3,
+    s1,s3,src1,src3,source_type,
+    r1t,r3t,
+    s1t,s3t,
+    lp,nPML,Rc,
+    C,
+    plot_interval,plot_source,
+    path,
+    save_wavefield);
+end
+## end
+    #=
+    write to gif
+    sources=[path 'pic/'];
+    delaytime=.2;
+    filename=['animation'];
+    gifmaker(filename,delaytime,sources);
+    # write recordings
+    ## recording saving
+    if ~exist([path '/rec/'],'dir')
+    mkdir([path '/rec/'])
+end
+
+DATA=rec_conversion(nt,nx,nz,dt,dx,dz,s1,s3,r1,r3);
+parsave([path '/rec/simu_info.mat'],DATA);
+
+DATA=R1;
+parsave([path '/rec/tR1.mat'],DATA);
+DATA=R3;
+parsave([path '/rec/tR3.mat'],DATA);
+DATA=P;
+parsave([path '/rec/tP.mat'],DATA);
+%% source saving
+%% recording saving
+if ~exist([path '/source/'],'dir')
+mkdir([path '/source/'])
+end
+DATA=src1;
+parsave([path '/source/src1.mat'],DATA);
+DATA=src3;
+parsave([path '/source/src3.mat'],DATA);
+DATA=source_type;
+parsave([path '/source/source_type.mat'],DATA);
+end
+toc;
+=#
---
+++ seismic2D_function.jl
@@ -0,0 +1,335 @@
+function meshgrid(x,y)
+    x2=zeros(length(x),length(y));
+    y2=x2;
+    x2=repeat(x,1,length(y));
+    y2=repeat(reshape(y,1,length(y)),length(x),1);
+    return x2,y2
+end
+
+function write2mat(path,var)
+    file=matopen(path,"w");
+    write(file,"data",data);
+    close(file);
+    return 0
+end
+
+function rickerWave(freq,dt,ns,M)
+    ## calculate scale
+    E=10 .^(5.24+1.44 .*M);
+    s=sqrt(E.*freq/.299);
+
+    t=dt:dt:dt*ns;
+    t0=1 ./freq;
+    t=t .-t0;
+    ricker=s .*(1 .-2*pi^2*freq .^2*t .^2).*exp.(-pi^2*freq^2 .*t .^2);
+    ricker=ricker;
+    ricker=Float32.(ricker);
+    return ricker
+end
+##
+@parallel function compute_sigma(dt,dx,dz,C11,C13,C33,C55,beta,v1,v1_3_2_end,v3,v3_1_2_end,
+    sigmas11,sigmas13,sigmas33,p)
+
+    @inn(sigmas11)=dt*.5*((@all(C11)-@all(C13)) .*@d_xi(v1)/dx+
+    (@all(C13)-@all(C33)) .*@d_yi(v3)/dz)+
+    @inn(sigmas11)-
+    dt*@all(beta) .*@inn(sigmas11);
+
+    @inn(sigmas33)=dt*.5*((@all(C33)-@all(C13)) .*@d_yi(v3)/dz+
+    (@all(C13)-@all(C11)) .*@d_xi(v1)/dx)+
+    @inn(sigmas33)-
+    dt*@all(beta).*@inn(sigmas33);
+
+    @inn(sigmas13)=dt*(@all(C55) .*(@d_yi(v1_3_2_end)/dz+@d_xi(v3_1_2_end)/dx))+
+    @inn(sigmas13)-
+    dt*@all(beta).*@inn(sigmas13);
+
+    # p
+    @inn(p)=-dt*((@all(C11)+@all(C33))*.5 .*@d_xi(v1)/dx+
+    (@all(C13)+@all(C33))*.5 .*@d_yi(v3)/dz)+
+    @inn(p)-
+    dt*@all(beta).*@inn(p);
+
+    return nothing
+end
+##
+@parallel function compute_v(dt,dx,dz,rho,v1,v3,beta,sigmas11_minus_p_1_2_end,
+    sigmas13,sigmas33_minus_p_3_2_end)
+
+    @inn(v1)=dt./@all(rho) .*(@d_xi(sigmas11_minus_p_1_2_end)/dx+
+    @d_yi(sigmas13)/dz)+
+    @inn(v1)-
+    dt*@all(beta) .*@inn(v1);
+
+    @inn(v3)=dt./@all(rho) .*(@d_xi(sigmas13)/dx+
+    @d_yi(sigmas33_minus_p_3_2_end)/dz)+
+    @inn(v3)-
+    dt*@all(beta) .*@inn(v3);
+    return nothing
+end
+
+@parallel function add_P_source(dx,dz,rho,v1,v3,ts_1_2_end,ts2_3_2_end)
+    @inn(v1)=@inn(v1)+.5 ./@all(rho) .*@d_xi(ts_1_2_end)/dx;
+    @inn(v3)=@inn(v3)+.5 ./@all(rho) .*@d_yi(ts2_3_2_end)/dz;
+    return nothing
+end
+
+function VTI_2D(dt,dx,dz,nt,nx,nz,
+    X,Z,
+    r1,r3,
+    s1,s3,src1,src3,source_type,
+    r1t,r3t,
+    s1t,s3t,
+    lp,nPML,Rc,
+    C,
+    plot_interval,plot_source,
+    path,
+    save_wavefield)
+
+    d0=Dates.now();
+
+    #create folder for figures
+    if isdir(path)==0
+        mkdir(path);
+    end
+
+    n_picture=1;
+    if save_wavefield==1
+        if isdir(string(path,"/forward_wavefield/"))==0;
+            mkdir(string(path,"/forward_wavefield/"));
+        end
+    end
+
+    if plot_interval!=0
+        if isdir(string(path,"/forward_pic/"))==0
+            mkdir(string(path,"/forward_pic/"))
+        end
+    end
+
+    # PML
+    vmax=sqrt.((C.C33) ./C.rho);
+    beta0=(ones(nx,nz) .*vmax .*(nPML+1) .*log(1/Rc)/2/lp/dx);
+    beta1=(@zeros(nx,nz));
+    beta3=beta1;
+    tt=(1:lp)/lp;
+    tt2=repeat(reshape(tt,lp,1),1,nz);
+    plane_grad1=@zeros(nx,nz);
+    plane_grad3=plane_grad1;
+
+    plane_grad1[2:lp+1,:]=reverse(tt2,dims=1);
+    plane_grad1[nx-lp:end-1,:]=tt2;
+    plane_grad1[1,:]=plane_grad1[2,:];
+    plane_grad1[end,:]=plane_grad1[end-1,:];
+
+    tt2=repeat(reshape(tt,1,lp),nx,1);
+    plane_grad3[:,2:lp+1]=reverse(tt2,dims=2);
+    plane_grad3[:,nz-lp:end-1]=tt2;
+    plane_grad3[:,1]=plane_grad3[:,2];
+    plane_grad3[:,end]=plane_grad3[:,end-1];
+
+    beta1=beta0.*plane_grad1.^nPML;
+    beta3=beta0.*plane_grad3.^nPML;
+
+    IND=unique(findall(f-> f!=0,beta1.*beta3));
+    beta=beta1+beta3;
+    beta[IND]=beta[IND]/2;
+
+    beta1=beta3=plane_grad1=plane_grad3=vmax=nothing;
+
+    # receiver configuration
+    R1=@zeros(nt,length(r1));
+    R3=@zeros(nt,length(r3));
+    P=@zeros(nt,length(r3));
+
+    # wave vector
+    v1=@zeros(nx,nz);
+    v3=@zeros(nx,nz);
+
+    sigmas11=@zeros(nx,nz);
+    sigmas13=@zeros(nx,nz);
+    sigmas33=@zeros(nx,nz);
+    p=@zeros(nx,nz);
+    ts=@zeros(nx,nz);
+    ts2=@zeros(nx,nz);
+
+    l=1;
+    ts[CartesianIndex.(s1,s3)]=src1[1];
+    ts2[CartesianIndex.(s1,s3)]=src3[1];
+
+    if source_type=='D'
+        v1=v1+.5 ./C.rho .*ts;
+        v3=v3+.5 ./C.rho .*ts2;
+    end
+    if source_type=='P'
+        @parallel add_P_source(dx,dz,C.rho,v1,v3,ts[2:end,:],ts2[:,2:end]);
+    end
+
+    ## save wavefield
+    if save_wavefield==1
+        data=zeros(nx,nz);
+        write2mat(string(path,"/forward_wavefield/v1_",l,".mat"),data);
+        data=zeros(nx,nz);
+        write2ma(string(path,"/forward_wavefield/v3_",l,".mat"),data);
+        data=zeros(nx,nz);
+        write2mat(string(path,"/forward_wavefield/sigmas11_",l,".mat"),data);
+        data=zeros(nx,nz);
+        write2mat(string(path,"/forward_wavefield/sigmas33_",l,".mat"),data);
+        data=zeros(nx,nz);
+        write2mat(string(path,"/forward_wavefield/sigmas13_",l,".mat"),data);
+        data=zeros(nx,nz);
+        write2mat(string(path,"/forward_wavefield/p_",l,".mat"),data);
+
+        data=v1;
+        write2mat(string(path,"/forward_wavefield/v1_",l+1,".mat"),data);
+        data=v3;
+        write2mat(string(path,"/forward_wavefield/v3_",l+1,".mat"),data);
+        data=sigmas11;
+        write2mat(string(path,"/forward_wavefield/sigmas11_",l+1,".mat"),data);
+        data=sigmas33;
+        write2mat(string(path,"/forward_wavefield/sigmas33_",l+1,".mat"),data);
+        data=sigmas13;
+        write2mat(string(path,"/forward_wavefield/sigmas13_",l+1,".mat"),data);
+        data=p;
+        write2mat(string(path,"/forward_wavefield/p_",l+1,".mat"),data);
+    end
+    ##
+
+    for l=2:nt-1
+        v1_3_2_end=v1[:,2:end];
+        v3_1_2_end=v3[2:end,:];
+        @parallel compute_sigma(dt,dx,dz,C.C11,C.C13,C.C33,C.C55,beta,v1,v1[:,2:end],
+        v3,v3[2:end,:],
+        sigmas11,sigmas13,sigmas33,p);
+
+        sigmas11_minus_p_1_2_end=sigmas11[2:end,:]-p[2:end,:];
+        sigmas33_minus_p_3_2_end=sigmas33[:,2:end]-p[:,2:end];
+
+        @parallel compute_v(dt,dx,dz,rho,v1,v3,beta,sigmas11_minus_p_1_2_end,
+        sigmas13,sigmas33_minus_p_3_2_end);
+
+        ts[CartesianIndex.(s1,s3)]=src1[l];
+        ts2[CartesianIndex.(s1,s3)]=src3[l];
+
+        if source_type=='D'
+            v1=v1+.5 ./C.rho .*ts;
+            v3=v3+.5 ./C.rho .*ts2;
+        end
+        if source_type=='P'
+                @parallel add_P_source(dx,dz,C.rho,v1,v3,ts[2:end,:],ts2[:,2:end]);
+        end
+
+        # assign recordings
+        R1[l+1,:].=v1[CartesianIndex.(r1,r3)];
+        R3[l+1,:].=v3[CartesianIndex.(r1,r3)];
+        P[l+1,:].=p[CartesianIndex.(r1,r3)];
+        # save wavefield
+        if save_wavefield==1
+            data=v1;
+            write2mat(string(path,"/forward_wavefield/v1_",l+1,".mat"),data);
+            data=v3;
+            write2mat(string(path,"/forward_wavefield/v3_",l+1,".mat"),data);
+            data=sigmas11;
+            write2mat(string(path,"/forward_wavefield/sigmas11_",l+1,".mat"),data);
+            data=sigmas33;
+            write2mat(string(path,"/forward_wavefield/sigmas33_",l+1,".mat"),data);
+            data=sigmas13;
+            write2mat(string(path,"/forward_wavefield/sigmas13_",l+1,".mat"),data);
+            data=p;
+            write2mat(string(path,"/forward_wavefield/p_",l+1,".mat"),data);
+        end
+        l2=l;
+        # plot
+        if plot_interval!=0
+            if mod(l,plot_interval)==0 || l==nt-1
+                mat"
+                hfig=figure('Visible','off');
+                set(hfig,'position',[0,0,1200,600]);
+                subplot(2,3,1)
+                imagesc([min($X,[],'all'),max($X,[],'all')],[max($Z,[],'all'),min($Z,[],'all')],$v3');
+                axis on;
+                hold on;
+                set(gca,'ydir','normal');
+                colorbar;
+                xlabel({['x [m]']});
+                ylabel({['z [m]']});
+                title({['t=' num2str($l2*$dt) 's'],['v_3 [m/s]']});
+                xlabel('x [m]');
+                ylabel('z [m]');
+                colorbar;
+                hold on;
+                ax2=plot($s1t,$s3t,'v','color',[1,0,0]);
+                hold on;
+                ax4=plot($r1t,$r3t,'^','color',[0,1,1]);
+
+                subplot(2,3,2)
+                imagesc([min($X,[],'all'),max($X,[],'all')],[max($Z,[],'all'),min($Z,[],'all')],$sigmas33');
+                axis on;
+                set(gca,'ydir','normal');
+                xlabel('x [m]');
+                ylabel('z [m]');
+                title('sigmas33 [Pa]');
+                colorbar;
+
+                subplot(2,3,3)
+                imagesc([min($X,[],'all'),max($X,[],'all')],[max($Z,[],'all'),min($Z,[],'all')],$sigmas13');
+                axis on;
+                set(gca,'ydir','normal');
+                xlabel('x [m]');
+                ylabel('z [m]');
+                title('sigmas13 [Pa]');
+                colorbar;
+
+                subplot(2,3,4)
+                imagesc([min($X,[],'all'),max($X,[],'all')],[max($Z,[],'all'),min($Z,[],'all')],$p');
+                axis on;
+                set(gca,'ydir','normal');
+                xlabel('x [m]');
+                ylabel('z [m]');
+                title('p [Pa]');
+                colorbar;
+
+                subplot(2,3,5)
+                axis on;
+                imagesc([1,length($r1)],[1,($l2+1)]*$dt,$R3(1:$l2+1,:));
+                colorbar;
+                xlabel('Nr');
+                ylabel('t [s]');
+                title('R3 [m/s]');
+                ylim([1,$nt]*$dt);
+
+                subplot(2,3,6)
+                axis on;
+                imagesc([min($X,[],'all'),max($X,[],'all')],[max($Z,[],'all'),min($Z,[],'all')],$C.C33');
+                set(gca,'ydir','normal');
+                xlabel('x [m]');
+                ylabel('z [m]');
+                title('C33 [Pa]');
+                colorbar;
+                hold on;
+                ax2=plot($s1t,$s3t,'v','color',[1,0,0]);
+                hold on;
+                ax4=plot($r1t,$r3t,'^','color',[0,1,1]);
+
+                legend([ax2,ax4],...
+                'source','receiver',...
+                'Location',[0.5,0.02,0.005,0.002],'orientation','horizontal');
+
+                print(gcf,[$path './forward_pic/' num2str($n_picture) '.png'],'-dpng','-r200');
+                "
+                n_picture=n_picture+1;
+            end
+        end
+        #=
+        fprintf('\n time step=%d/%d',l+1,nt);
+        fprintf('\n    epalsed time=%.2fs',toc);
+        fprintf('\n    n_picture=%d',n_picture);
+        d=clock;
+        fprintf('\n    current time=%d %d %d %d %d %.0d',d(1),d(2),d(3),d(4),d(5),d(6));
+        =#
+        d=Dates.now();
+        println("\n time step=",l+1,"/",nt);
+        println("\n    n_picture=",n_picture);
+        println("\n    ",d);
+    end
+    return v1,v3,R1,R3,P
+end
